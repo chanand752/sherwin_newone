3c386630acfbc152c84c675797ddc82a3e05e914
Chavali Anand Babu <87194197+chanand752@users.noreply.github.com>
2022-04-14 17:47:13 +0530

    api added

--- sherwin_demo/src/AdminDashBoard/BrandQualityGroup/SearchBrandQuality.js
+++ sherwin_demo/src/AdminDashBoard/BrandQualityGroup/SearchBrandQuality.js
@@ -1,8 +1,9 @@
 // In Search Brand Quality
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect, memo } from 'react';
 import Box from '@mui/material/Box';
 import { Button } from '@mui/material';
+import axios from 'axios';
 import { TextField, TableCell, TableBody, Table, TableRow, TableContainer, TableHead, Paper, tableCellClasses, styled, Alert, Stack, MuiAlert } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 
@@ -12,38 +13,38 @@ import CloseIcon from '@mui/icons-material/Close';
 import './BrandQuality.css';
 
 
-export default function SearchBrandQuality() {
+function SearchBrandQuality() {
   const [open, setOpen] = useState(false);
   const [open1, setOpen1] = useState(false);
   const [open2, setOpen2] = React.useState(false);
 
+  ///////////////////////////////////////////////////////////
 
-// delete dialogo method code
-
-const handleClickOpen2 = () => {
-  setOpen2(true);
-};
-
-const handleClose2 = () => {
-  setOpen2(false);
-};
+  const [discription, setCreateDiscription] = React.useState(null)
+  const [code, setCreateCode] = React.useState()
+  const [state, setState] = useState()
+  const [data, setData] = React.useState([])
+  const [loading, setLoading] = useState(false)
 
 
 
 
 
+  // delete dialogo method code
 
+  const handleClickOpen2 = () => {
+    setOpen2(true);
+  };
 
-
-
-
-
+  const handleClose2 = () => {
+    setOpen2(false);
+  };
 
   ////////////////////////////////////////////////////////////////////////////
 
-  const handleClick1 = () => {
-    setOpen1(true)
-  };
+  // const handleClick1 = () => {
+  //   setOpen1(true)
+  // };
 
   const handleClose1 = (event, reason) => {
     if (reason === 'clickaway') {
@@ -82,10 +83,23 @@ const handleClose2 = () => {
 
 
   };
+  // post Method
+
   const handleCreate = () => {
     setOpen(false);
-   
+    const article = { code, discription };
+    axios.post('http://172.17.12.42:3020/data/createqualitygroup', article)
+      .then(res => {
+        console.log(res)
+        setState(res.article);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    console.log('Created')
+    // window.location.reload(false);
   }
+
 
   //Table
 
@@ -111,33 +125,106 @@ const handleClose2 = () => {
     },
   }));
 
-  function createData(code, discription, brand, carbs) {
-    return { code, discription, brand, carbs };
-  }
+  // function createData(code, discription, brand) {
+  //   return { code, discription, brand };
+  // }
 
   const handleDelete = (code, e) => {
-    e.target.parentNode.parentNode.parentNode.deleteRow(code)
-    alert('Selcted Row deleted')
-    console.log('deleted');
+    e.preventDefault();
+  
+    axios.delete(`http://172.17.12.42:3020/data/deletedataa`, {"data" : { "code" : code }}).then(
+      res => console.log("Deleted!!!", res)
+    ).catch(
+      err => console.log(err)
+    )
+    // console.log('deleted');
+    // window.location.reload(false);
+    setOpen1(true)
     setOpen2(false)
-   
+    
   }
 
 
-  const rows = [
-    createData('AD1411', 'AD1411 LAQVIN PRIME', 'Sherwin-Williams Product Finishes Europe'),
-    createData('AFL31', 'AFL31 HYDROPLUS SELF SEALER', 'Sherwin-Williams Product Finishes Europe'),
-    createData('AD1430', 'AD1430 LAQVIN PRIME', 'Sherwin-Williams Product Finishes Europe'),
-    createData('AF72', 'AF72 HYDRO WATERBORNE CLEAR SELF-SEALER FOR INTERIORS', 'Sherwin-Williams Product Finishes Europe'),
-    createData('AF74', 'AF74 HYDRO WATERBORNE CLEAR SELF-SEALER FOR INTERIORS', 'Sherwin-Williams Product Finishes Europe'),
-  ];
+  // const rows = [
+  //   createData('AD1411', 'AD1411 LAQVIN PRIME', 'Sherwin-Williams Product Finishes Europe'),
+  //   createData('AFL31', 'AFL31 HYDROPLUS SELF SEALER', 'Sherwin-Williams Product Finishes Europe'),
+  //   createData('AD1430', 'AD1430 LAQVIN PRIME', 'Sherwin-Williams Product Finishes Europe'),
+  //   createData('AF72', 'AF72 HYDRO WATERBORNE CLEAR SELF-SEALER FOR INTERIORS', 'Sherwin-Williams Product Finishes Europe'),
+  //   createData('AF74', 'AF74 HYDRO WATERBORNE CLEAR SELF-SEALER FOR INTERIORS', 'Sherwin-Williams Product Finishes Europe'),
+  // ];
+
+
+
+
+
+
+  function loader() {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+  }
+
+
+  useEffect(() => {
+    axios.get("http://172.17.12.42:3020/data/getdata", {
+      headers: {
+        'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'
+      }
+    }
+    )
+      .then(res => {
+        console.log(res.data);
+        setData(res.data)
+        loader();
+      })
+
+  }, []);
+
+
+  function handleInputChange1(event) {
+    setCreateCode(event.target.value);
+  }
+
+  function handleInputChange2(event) {
+    setCreateDiscription(event.target.value);
+  }
+
+
+  // function createBrand() {
+  //   alert(" " +  + " is selected for Updating")
+
+  //   axios.put(`http://172.17.12.42:4231/books/bookupdate`, { "":  }).then(
+  //     res => console.log("Updated!!!", res)
+  //   ).catch(
+  //     err => console.log(err)
+  //   )
+
+  // }
+
+
+  // function deleteBrand(code, e) {
+  //   alert("Brand " + code + " is selected for Delete")
+  //   e.preventDefault();
+
+  //   axios.delete(`http://172.17.12.42:4231/books/detailsdelete`, { "data": { "code": code } }).then(
+  //     res => console.log("Deleted!!!", res)
+  //   ).catch(
+  //     err => console.log(err)
+  //   )
+  //   Showbook(updateData);
+  // }
+
+
+
+
+
+
+
+
 
   const [search, setSearch] = React.useState('');
-  const data = rows.filter(a => a.discription.toLowerCase().includes(search.toLowerCase()))
-
-
-
-
+  const searchBycode = data.filter(a => a.discription.toLowerCase().includes(search.toLowerCase()) || a.code.toLowerCase().includes(search.toLowerCase()))
 
   return (
     <div className='body-brand-quality'>
@@ -154,7 +241,7 @@ const handleClose2 = () => {
         }}
       >
         <div>
-          <SearchIcon className='icon' /><TextField id="standard-basic" label="Filter Groups" variant="standard" value={search} onChange={(e) => setSearch(e.target.value)} />
+          <SearchIcon className='icon' /><TextField id="standard-basic" label="Search By Discription or Code" variant="standard" value={search} onChange={(e) => setSearch(e.target.value)} />
         </div>
 
         {/* <Button variant="contained" color="success" className='add-button' >Create New Quality Group</Button> */}
@@ -170,17 +257,22 @@ const handleClose2 = () => {
               label="Quality Group Code*"
               fullWidth
               variant="standard"
+              name='code'
+              value={code}
+              onChange={handleInputChange1}
 
             />
 
             <TextField
               autoFocus
               margin="dense"
-              id="code"
+              id="discription"
               label="Description*"
               fullWidth
               variant="standard"
-
+              name='discription'
+              value={discription}
+              onChange={handleInputChange2}
             />
 
           </DialogContent>
@@ -209,13 +301,13 @@ const handleClose2 = () => {
               </TableHead>
               <TableBody >
                 {
-                  data.map((b, code) =>
+                  searchBycode.map((data, code) =>
                     <StyledTableRow key={code}  >
                       <StyledTableCell component="th" scope="row"  >
-                        <b className='table-code'>{b.code}</b>
+                        <b className='table-code'>{data.code}</b>
                       </StyledTableCell>
-                      <StyledTableCell align="left"><b>{b.discription}</b></StyledTableCell>
-                      <StyledTableCell align="left"><b>{b.brand}</b></StyledTableCell>
+                      <StyledTableCell align="left"><b>{data.discription}</b></StyledTableCell>
+                      <StyledTableCell align="left"><b>{data.brand}</b></StyledTableCell>
                       <StyledTableCell align="center">
                         <Button variant="contained" color="success" className='Button-delete' onClick={handleClickOpen2} ><DeleteIcon /></Button>
                         {/* onClick={e => handleDelete(code, e)} */}
@@ -234,12 +326,12 @@ const handleClose2 = () => {
                             </DialogTitle>
                             <DialogContent>
                               <DialogContentText id="alert-dialog-description">
-                            
+
                               </DialogContentText>
                             </DialogContent>
                             <DialogActions>
                               <Button onClick={handleClose2}>No</Button>
-                              <Button onClick={handleClose2}>
+                              <Button onClick={e => handleDelete(data.code, e)}>
                                 Yes
                               </Button>
                             </DialogActions>
@@ -251,21 +343,29 @@ const handleClose2 = () => {
               </TableBody>
             </Table>
           </TableContainer>
-          {open1 === true &&
-            <Snackbar
-              open={open1}
-              autoHideDuration={2000}
-              onClose={handleClose1}
-              message="Note archived"
-              action={action}
-            />
-          }
-
         </div>
+        {open1 === true &&
+          //     <Snackbar>  
+          //       open={open1}
+          //       autoHideDuration={3000}
+          //       onClose={handleClose1}
+          //       {/* message="Deleted Succesfully" */}
+          //       <Alert onClose={handleClose1} severity="success" sx={{ width: '100%' }}>
+          //   This is a success message!
+          // </Alert>
 
+          //       </Snackbar>
+          <Snackbar open={open1} autoHideDuration={6000} onClose={handleClose1}>
+            <Alert onClose={handleClose1} severity="success" sx={{ width: '100%' }}>
+              Deleted success fully!
+            </Alert>
+          </Snackbar>
+        }
       </div>
 
     </div>
   );
 }
+
+export default memo (SearchBrandQuality);
 
